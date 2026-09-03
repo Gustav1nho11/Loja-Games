3e7dd36a6fc204a103ab22d8f49acaa1c8701142
Gustav1nho11 <161361912+Gustav1nho11@users.noreply.github.com>
2026-09-03 09:00:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 const games = [
-  { id: 1, title: 'Elden Ring', plataforma: 'PS5', genero: 'RPG', price: 'R$ 249,00' },
-  { id: 2, title: 'Cyberpunk 2077', plataforma: 'PC', genero: 'RPG', price: 'R$ 199,00' },
-  { id: 3, title: 'Hollow Knight', plataforma: 'PC', genero: 'Indie', price: 'R$ 46,99' },
-  { id: 4, title: 'Zelda: Tears of the Kingdom', plataforma: 'Switch', genero: 'Ação', price: 'R$ 349,00' },
-  { id: 5, title: 'God of War Ragnarök', plataforma: 'PS5', genero: 'Ação', price: 'R$ 299,00' }
+  { 
+    id: 1, 
+    title: 'Elden Ring', 
+    plataforma: 'PS5', 
+    genero: 'RPG', 
+    price: 'R$ 249,00',
+    cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC8f8XsY_dPgkrJxM2eOhOvA_1Mk0OvZJCJ5AbDXeh2w&s=10' 
+  },
+  { 
+    id: 2, 
+    title: 'Cyberpunk 2077', 
+    plataforma: 'PC', 
+    genero: 'RPG', 
+    price: 'R$ 199,00',
+    cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlnNfLfVXP5yFpVfP0t6fVHrvCHi6PNCxm3HeZZq8cLw&s=10' 
+  },
+  { 
+    id: 3, 
+    title: 'Hollow Knight', 
+    plataforma: 'PC', 
+    genero: 'Indie', 
+    price: 'R$ 46,99',
+    cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBM27fdmn1PKxuGaneuQ8CDxuUAC_75MqJFA-TlCYcnw&s=10' 
+  },
+  { 
+    id: 4, 
+    title: 'Zelda: Tears of the Kingdom', 
+    plataforma: 'Switch', 
+    genero: 'Ação', 
+    price: 'R$ 349,00',
+    cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQW4rRPAMpLZlZhBOVohZYQT9E8YGLxn2zKunONYKCyA&s' 
+  },
+  { 
+    id: 5, 
+    title: 'God of War Ragnarök', 
+    plataforma: 'PS5', 
+    genero: 'Ação', 
+    price: 'R$ 299,00',
+    cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgku4SxrbkSNKW_HscO1gIXRToGruEiZvOt00G1dbLzw&s=10' 
+  }
 ];
 
 const checkboxes = document.querySelectorAll('.filter-check');
@@ -54,6 +89,7 @@ function renderGames(filters) {
 
   gamesGrid.innerHTML = filteredGames.map(game => `
     <div class="card">
+      <img src="${game.cover}" alt="${game.title}" class="cover">
       <h4>${game.title}</h4>
       <p>Plataforma: ${game.plataforma}</p>
       <p>Gênero: ${game.genero}</p>
